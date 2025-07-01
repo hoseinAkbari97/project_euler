99eccd5432bf0f00953b0a8a4d7fae628ea0bc96
Hossein <hossein16akbari@gmail.com>
2025-07-01 17:19:54 +0330

    #3 done

--- 1-50/problem_3.py
+++ 1-50/problem_3.py
@@ -1,25 +1,19 @@
-def calculate_factors(number):
-    
-    """This function calculates the factors
-    of the given number"""
-    factors = []
-    for i in range(2, number/2):
-        if number % i == 0:
-            factors.append(i)
-            
-    return factors
+import math
 
-def calculate_prime_factors(factors):
-    
-    """This function calculates the prime elements 
-    of the given list"""
+def biggest_prime_factor(n):
+    factor = 2
+    last_factor = 1
+    while n > 1:
+        if n % factor == 0:
+            last_factor = factor
+            n = n // factor
+            while n % factor == 0:
+                n = n // factor
+        factor += 1
+        if factor * factor > n:
+            if n > 1:
+                last_factor = n
+                break
+    return last_factor
 
-
-def biggest_prime_factor(number):
-    
-    """This function calculates the biggest
-    prime factor of the given number"""
-    factors = calculate_factors(number)  
-    
-    prime_factors = calculate_prime_factors(factors)
-    
+print(biggest_prime_factor(600851475143))
