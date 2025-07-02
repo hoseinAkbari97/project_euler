9b4356c6f4edcb2a35d6cd555cc6e49d14cb5937
Hossein <hossein16akbari@gmail.com>
2025-07-02 15:26:22 +0330

    #4 Turning the number to separate numbers

--- 1-50/problem_4.py
+++ 1-50/problem_4.py
@@ -1,1 +1,16 @@
-998000
+def is_palindromic(number):
+
+    list = []
+
+    while True:
+        if number < 10:
+            list.insert(0, int(number))
+            return list
+        else:
+            rem = int(number % 10)
+            list.insert(0, rem)
+            number = number/10
+
+input = 1000
+print(is_palindromic(input))
+            
