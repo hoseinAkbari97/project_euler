b293910805d6734e0d0c8248a7fb92783909979e
Hossein <hossein16akbari@gmail.com>
2025-07-07 12:00:29 +0330

    #4 commenting out

--- 1-50/problem_4.py
+++ 1-50/problem_4.py
@@ -4,10 +4,10 @@ def is_palindrome(n):
 def largest_palindrome_product():
     max_palindrome = 0
     for i in range(999, 99, -1):
-        for j in range(i, 99, -1):  # Avoid duplicate pairs like (912, 913) and (913, 912)
+        for j in range(i, 99, -1):
             product = i * j
             if product <= max_palindrome:
-                break  # Products will get smaller; no need to continue
+                break 
             if is_palindrome(product):
                 max_palindrome = product
     return max_palindrome
